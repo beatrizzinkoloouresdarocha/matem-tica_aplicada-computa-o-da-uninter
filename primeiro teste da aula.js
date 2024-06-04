ef23040ba9448ee9b4b8a3c0fec39c857bea8b29
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-06-04 15:06:15 -0300

    //1exemplo).Para converter um número decimal para binário, você pode usar o método toString(2).

---
+++ primeiro teste da aula.js
@@ -0,0 +1,6 @@
+//1exemplo).Para converter um número decimal para binário, você pode usar o método toString(2).
+
+let decimalNumber = 42;
+let binaryString = decimalNumber.toString(2);
+console.log(binaryString); // Saída: "101010"
+
