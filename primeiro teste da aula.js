72ee78bdd8d8231d8fa81638f071bd6cd658dd1f
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-06-04 15:48:29 -0300

    fiz 10 exemplos de binária .

--- primeiro teste da aula.js
+++ primeiro teste da aula.js
@@ -4,3 +4,52 @@ let decimalNumber = 42;
 let binaryString = decimalNumber.toString(2);
 console.log(binaryString); // Saída: "101010"
 
+// 2º exemplo: 
+let decimalNumber2 = 15;
+let binaryString2 = decimalNumber2.toString(2);
+console.log(binaryString2); // Saída: "1111"
+
+// 3º exemplo: 
+let decimalNumber3 = 64;
+let binaryString3 = decimalNumber3.toString(2);
+console.log(binaryString3); // Saída: "1000000"
+
+// 4º exemplo: 
+let decimalNumber4 = 255;
+let binaryString4 = decimalNumber4.toString(2);
+console.log(binaryString4); // Saída: "11111111"
+
+// 5º exemplo: 
+let decimalNumber5 = 128;
+let binaryString5 = decimalNumber5.toString(2);
+console.log(binaryString5); // Saída: "10000000"
+
+// 6º exemplo: 
+let decimalNumber6 = 7;
+let binaryString6 = decimalNumber6.toString(2);
+console.log(binaryString6); // Saída: "111"
+
+// 7º exemplo: 
+let decimalNumber7 = 256;
+let binaryString7 = decimalNumber7.toString(2);
+console.log(binaryString7); // Saída: "100000000"
+
+// 8º exemplo: 
+let decimalNumber8 = 1023;
+let binaryString8 = decimalNumber8.toString(2);
+console.log(binaryString8); // Saída: "1111111111"
+
+// 9º exemplo: 
+let decimalNumber9 = 512;
+let binaryString9 = decimalNumber9.toString(2);
+console.log(binaryString9); // Saída: "1000000000"
+
+// 10º exemplo: 
+let decimalNumber10 = 2048;
+let binaryString10 = decimalNumber10.toString(2);
+console.log(binaryString10); // Saída: "100000000000"
+
+// 11º exemplo: 
+let decimalNumber11 = 31;
+let binaryString11 = decimalNumber11.toString(2);
+console.log(binaryString11); // Saída: "11111"
